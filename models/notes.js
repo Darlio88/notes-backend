823d4c2d6914ed4e266022d6a620c65d7c5b0aff
darlio88 <danieldarlio@gmail.com>
2022-09-05 18:22:28 +0300

    commit changes to final API

--- models/notes.js
+++ models/notes.js
@@ -4,7 +4,7 @@ const noteSchema = new mongoose.Schema({
     title:String,
     description:String,
     catergory:String,
-    createdAt:{type:Date, default:new Date()},
+    createdAt:{type:Date, immutable:true, default:new Date()},
     tags:[{type:String}],
     creatorId:String,
 })
